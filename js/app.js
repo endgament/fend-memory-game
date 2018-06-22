3b12a6c0aba5b2c67466e41f0f8bd33e160de791
Konstantina Tsarvouli <ntina_t@hotmail.com>
2018-06-22 10:20:41 +0300

    Adding function for flipping the cards over

--- js/app.js
+++ js/app.js
@@ -25,14 +25,28 @@ function shuffle(array) {
     return array;
 }
 
+ /* -------------FLIPPING THE CARDS -------------*/
+ 
+function flipCard() {
+    if (!timestarted) { // starting the timer just once and not with every flip
+        startTimer(); 
+        timestarted= true;
+    }
+    this.classList.toggle('open'); // open the card
+    this.classList.toggle('show'); // show the card's symbol
+    this.classList.toggle('noClick'); // remove the possibility of clicking the same card again
+    openCards.push(this);
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+    if (openCards.length === 2) { // checking if there are two cards open
+        moveCounter();
+        if (openCards[0].innerHTML === openCards[1].innerHTML) { //+ if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+            matchCards(openCards[0]);
+            matchCards(openCards[1]);
+            openCards = [];
+        }  
+        else { //+ if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+            setTimeout(noMatch, 600);
+        }       
+    }
+    gameOver();    
+}
